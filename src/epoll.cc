f1df2fd7549857539bb639b6260b50cc04de34e1
liukang1995 <57588463@qq.com>
2020-07-07 21:21:04 +0800

    epoll add timer fun

--- src/epoll.cc
+++ src/epoll.cc
@@ -3,6 +3,8 @@
 #include "epoll.h"
 #include "log/logger.h"
 
+#include "unistd.h"
+
 #include "assert.h"
 
 using namespace summer;
@@ -10,38 +12,34 @@ using namespace std;
 
 
 Epoll::Epoll()
-:epfd_( epoll_create1(EPOLL_CLOEXEC) ),
-active_list(4096)
+    :epfd_( epoll_create1(EPOLL_CLOEXEC) ),
+    active_list(4096),
+    manage_()
 {
     assert( epfd_ > 0 );
 }
 
-Epoll::~Epoll( ) { }
+Epoll::~Epoll( ) { close(epfd_); }
 
 void Epoll::epoll_add(Channel_Ptr fdptr, int timeout )
 {
     int fd = fdptr->getFd();
-    /*
+    
+    //timeout秒之后，将fdptr从epoll监听的列表上剥离
+    if(timeout > 0)
+        manage_.addtimer(fdptr->getholder(),timeout,[ptr = fdptr->getholder().lock()]
+                                                {
+                                                    if( ptr ){
+                                                        ptr->handleclose();
+                                                    }
+                                                });
 
-    add timer;
-
-
-
-    */
-
-   struct epoll_event event;
-   event.data.fd = fd;
-   event.events = fdptr->getEvents();
-
-   /*
-
-
-
-   update fdptr->event?
-
-   */
-
-  // channels用于管理已经监听的fd
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = fdptr->getEvents();
+    //recode this change, for modify in future
+    fdptr->compare_update_lastevents();
+    // channels用于管理已经监听的fd
     channels[fd] = fdptr;
 
     if ( epoll_ctl(epfd_, EPOLL_CTL_ADD, fd, &event ) < 0 ){
@@ -54,20 +52,26 @@ void Epoll::epoll_mod(Channel_Ptr fdptr, int timeout )
 {
     int fd = fdptr->getFd();
 
-    // 必然存在
+    // 已在监听
     assert( channels.find(fd) != channels.end() );
-
-    if ( timeout > 0 )
+    // 需要更新计时器，未生效的timernode执为实效，并重新添加新节点
+    if ( timeout >= 0 )
     {
-        /*
-
-        update timeer
-
-
-        */
+        auto p = fdptr->getholder().lock();
+        if( p ){
+           auto t = p->gettimer().lock();
+           if( t ) t->setdeleted();
+           if( timeout > 0)
+                manage_.addtimer(fdptr->getholder(),timeout,[ptr = fdptr->getholder().lock()]
+                                                    {
+                                                        if( ptr ){
+                                                            ptr->handleclose();
+                                                        }
+                                                    });
+        }
     }
 
-    // 需要更新
+    // 需要更新事件
     if( !fdptr->compare_update_lastevents() )
     {
         struct epoll_event event;
@@ -76,18 +80,15 @@ void Epoll::epoll_mod(Channel_Ptr fdptr, int timeout )
 
         if ( epoll_ctl( epfd_, EPOLL_CTL_MOD, fd, &event) < 0 ){
             LOGERROR << " EPOLL MOD ERROR, ADD RESET TO LASTEVENTS ";
-            
         }
     }
 }
 
 void Epoll::epoll_rm( Channel_Ptr fdptr )
 {
-    /*
-
-    del timer?
-
-    */
+    auto p = fdptr->getholder().lock();
+    if(p)
+        p->canceltimer();
 
     int fd = fdptr->getFd();
     map<int,Channel_Ptr>::iterator it = channels.find(fd);
@@ -103,8 +104,6 @@ void Epoll::epoll_rm( Channel_Ptr fdptr )
         LOGERROR << "EPOLL DEL ERR. ";
         return;
     }
-
-    // 内核内产出成功，移除Epoll内的副本
     channels.erase( it );
 }
 
@@ -115,7 +114,6 @@ std::vector<Epoll::Channel_Ptr> Epoll::wait()
         int count = epoll_wait( epfd_, &*active_list.begin(),4096,-1);
         if ( count < 0 )
             LOGERROR << "EPOLL WAIT ERROR. ";
-        assert( count >= 0 );
 
         // 根据active_list返回vector<Channel_ptr> 
         vector<Channel_Ptr> active_ptrs;
@@ -125,12 +123,8 @@ std::vector<Epoll::Channel_Ptr> Epoll::wait()
             int fd = active_list[i].data.fd;
 
             Channel_Ptr fd_ptr = channels[fd];
-            assert( fd_ptr );
-
             fd_ptr->setrEvents( active_list[i].events );
-
             //fd_ptr->setEvents( 0 )?
-
             active_ptrs.push_back(fd_ptr);
         }
         if ( count > 0 )
--- src/epoll.h
+++ src/epoll.h
@@ -6,6 +6,7 @@
 #include "sys/epoll.h"
 
 #include "channel.h"
+#include "timer.h"
 
 #include <map>
 #include <memory>
@@ -16,32 +17,30 @@ namespace summer
 
     class Epoll 
     {
+    public:
+        using Channel_Ptr = std::shared_ptr<channel> ;
+        using Channel_List = std::vector<Channel_Ptr> ;
+
     private:
         // 持有的epoll文件描述符，在初始化时创建
         int epfd_;
-
-        typedef std::shared_ptr<channel> Channel_Ptr;
-        typedef std::vector<Channel_Ptr> Channel_List;
-
-        // 用于保存epoll_wait()活动的事件
+        // as return value of epoll_wait:  active fd
         std::vector<epoll_event> active_list;
-
         // 保存epfd监听的文件描述符
         std::map<int, Channel_Ptr> channels;
+        summer::timerManager manage_;
 
     public:
         Epoll();
         ~Epoll();
 
-        // 添加监听fd
         void epoll_add(Channel_Ptr fdptr, int timeout );
-
-        // 修改fd
         void epoll_mod(Channel_Ptr fdptr, int timeout );
+        void epoll_rm(Channel_Ptr fdptr );
 
-        // 删除fd
-        void epoll_rm( Channel_Ptr fdptr );
+        void handle_expired(){ manage_.handleExpired(); }
 
+        //核心
         std::vector<Channel_Ptr> wait();
 
         int getEpfd() { return epfd_; }
