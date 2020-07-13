835687ace638d18d880c359ba5591c794685405b
liukang1995 <57588463@qq.com>
2020-07-13 11:50:50 +0800

    re construct threadpool

---
+++ new_src/threadpool.cc
@@ -0,0 +1,41 @@
+#include "threadpool.h"
+
+using namespace std;
+using namespace summer;
+
+threadpool::threadpool(eventloop* baseloop, int numthread)
+    :
+    start_(false),
+    next_(0),
+    numthreads_(numthread),
+    baseloop_(baseloop),
+    loops_(),
+    threads_()
+{
+
+}
+
+// called by server
+void threadpool::start()
+{
+    start_ = true;
+    for( int i = 0; i < numthreads_; i++ )
+    {
+        shared_ptr<loopthread> elt( new loopthread() );
+        threads_.push_back(elt);
+        loops_.push_back( elt->start() );
+    }
+}
+
+// called by owner
+// 如果线程池中没有线程，则返回主loop，否则返回线程池中的下一loop
+summer::eventloop* threadpool::getnextloop()
+{
+    eventloop* loop = baseloop_;
+    if( !loops_.empty() )
+    {
+        loop = loops_[next_];
+        next_ = ( next_ + 1 ) % numthreads_;    
+    }
+    return loop;
+}
---
+++ new_src/threadpool.h
@@ -0,0 +1,34 @@
+//author：liukang
+//thread pool
+#pragma once
+
+#include "eventloop.h"
+#include "loopthread.h"
+
+#include <memory>
+#include <vector>
+
+
+namespace summer
+{
+    class threadpool
+    {
+    public:
+        threadpool( eventloop* baseloop, int numthreads );
+        threadpool(const threadpool&) = delete;
+        threadpool& operator=(const threadpool&) = delete;
+
+        ~threadpool() = default;
+
+        void start();
+        summer::eventloop* getnextloop();
+    
+    private:
+        bool start_;
+        int next_;
+        int numthreads_;
+        eventloop* baseloop_;
+        std::vector<eventloop*> loops_;
+        std::vector<std::shared_ptr<loopthread>> threads_;
+    };
+} // namespace summer
